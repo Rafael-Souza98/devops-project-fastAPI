8b40136a6aa58cc10c073f917b89bdfcb7e57cec
Rafael Souza <faelsos98@gmail.com>
2024-03-10 21:14:24 -0300

    fix: corrigi lib descontinuada flask_mongoengine

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM python:3.12.2-alpine3.19
 
-EXPOSE 8000
+EXPOSE 5000
 
 WORKDIR /app
 
+RUN apk add git 
+
 COPY requirements.txt .
 
-RUN pip install -r requirements.txt
+RUN pip3 install -r requirements.txt
 
 COPY main.py .
 
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from flask_mongoengine import MongoEngine
-
+from mongoengine import *
 
 app = Flask(__name__)
-api = Api(app)
-db = MongoEngine(app)
-
 
 app.config['MONGO_SETTINGS'] = {
     'db' : 'users',
@@ -16,6 +13,24 @@ app.config['MONGO_SETTINGS'] = {
     'password' : 'admin'
 }
 
+
+api = Api(app)
+db = MongoEngine(app)
+
+class User_Model(db.Document):
+    cpf = StringField(required=True, unique=True, max_lenght=50)
+    first_name = StringField(required=True)
+    last_name = StringField(required=True)
+    email = EmailField(required=True)
+    birth_date = DateTimeField(required=True)
+    
+
+
 class User(Resource):
     def get(self):
-        return {"message" : "user 1"}
+        return jsonify(User_Model._object_key) #corrigir
+    
+api.add_resource(User, "/user")
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, debug=True)
