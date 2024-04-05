8e9223a39dd23f83f7236a5dc8bf4d6da92cf409
Rafael Souza <faelsos98@gmail.com>
2024-04-05 11:34:50 -0300

    :test_tube: test: test birthday_date corrected

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,11 +1,13 @@
-from sqlite3 import connect
-import warnings
+
 import pytest
 from application import create_app
-import mongomock
+import datetime
+import pytz
 
 
 class TestApplication():
+
+    
 
     @pytest.fixture
     def client(self):
@@ -19,7 +21,7 @@ class TestApplication():
                 "last_name": "souza",
                 "cpf": "440.513.740-41",
                 "email": "faelsos98@gmail.com",
-                "birthday_date": "1998-02-12"
+                "birthday_date": "1998-02-10"
                 }
     @pytest.fixture
     def invalid_user(self):
@@ -28,7 +30,7 @@ class TestApplication():
                 "last_name": "souza",
                 "cpf": "440.513.740-21",
                 "email": "faelsos98@gmail.com",
-                "birthday_date": "1998-02-12"
+                "birthday_date": "1998-02-10"
                 }
 
     def test_get_users(self, client):
@@ -46,11 +48,20 @@ class TestApplication():
     
     def test_get_user_by_cpf(self, client, valid_user, invalid_user):
         resp = client.get('/user/%s' % valid_user['cpf'] )
+
+        timestamp_ms = resp.json[0]["birthday_date"]["$date"]
+        # Convertendo o timestamp para um objeto datetime usando o fuso horário da região us-west-2
+        
+        birthday_date = datetime.datetime.fromtimestamp(timestamp_ms / 1000.0)
+        # Formatando a data como uma string no formato desejado
+        formatted_birthday_date = birthday_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+
         assert resp.status_code == 200
         assert resp.json[0]["first_name"] == "rafael"
         assert resp.json[0]["last_name"] == "souza"
         assert resp.json[0]["cpf"] == "440.513.740-41"
-        assert resp.json[0]["birthday_date"]["$date"] == '1998-02-12T00:00:00Z'
+        assert formatted_birthday_date == '1998-02-10T22:00:00Z'
 
         resp = client.get('/user/%s' % invalid_user['cpf'] )
         assert resp.status_code == 400
