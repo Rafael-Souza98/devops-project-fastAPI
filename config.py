be46771e5f452de76ac849919ebfcbf5427a0a1c
Rafael Souza <faelsos98@gmail.com>
2024-04-22 21:44:45 -0300

    fix: error in url connection

--- config.py
+++ config.py
@@ -13,11 +13,11 @@ class ProdConfig():
     MONGODB_USER = os.getenv('MONGODB_USER')
     MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
     MONGODB_HOST = os.getenv('MONGODB_HOST')
-    # MONGODB_DB = os.getenv('MONGODB_DB')
+    MONGODB_DB = os.getenv('MONGODB_DB')
 
     MONGODB_SETTINGS = {
-        'db' : 'users',
-        'host': "mongodb+srv://%s:%s@%s/" % (
+        'db': 'users',
+        'host': "mongodb://%s:%s@%s/?retryWrites=true&w=majority&appName=db-app-python" % (
             MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST
         )
     }
